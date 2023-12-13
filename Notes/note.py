3de48f9fe636b2d764a6128a0a20240a5af75a42
mox3r <mox3r.box@gmail.com>
2023-12-13 19:33:59 +0300

    complete

---
+++ Notes/note.py
@@ -0,0 +1,45 @@
+import datetime
+
+
+class Note(object):
+
+    def __init__(self, note_id, date, title, text):
+        self.note_id = note_id
+        self.date = date
+        self.title = title
+        self.text = text
+
+    @property
+    def note_id(self):
+        return self._note_id
+
+    @note_id.setter
+    def note_id(self, note_id):
+        self._note_id = note_id
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, date):
+        self._date = date
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, text):
+        self._text = text
+
+    def __str__(self):
+        return f'\n#{self._note_id}\n{self._title}: {self._text}\nПоследнее изменение: {self._date}'
